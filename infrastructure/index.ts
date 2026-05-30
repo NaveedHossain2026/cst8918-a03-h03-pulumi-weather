334550fc69d513820312d3b31adf4893bc6c47e6
NaveedHossain2026 <hoss0113@algonquinlive.com>
2026-05-30 17:19:21 -0400

    infra: complete registry baseline setup and verify azure connections

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,45 @@
 import * as pulumi from "@pulumi/pulumi";
+
+import * as resources from '@pulumi/azure-native/resources'
+import * as containerregistry from '@pulumi/azure-native/containerregistry'
+
+
+const config = new pulumi.Config()
+const appPath = config.require('appPath')
+const prefixName = config.require('prefixName')
+const imageName = prefixName
+const imageTag = config.require('imageTag')
+// Azure container instances (ACI) service does not yet support port mapping
+// so, the containerPort and publicPort must be the same
+const containerPort = config.requireNumber('containerPort')
+const publicPort = config.requireNumber('publicPort')
+const cpu = config.requireNumber('cpu')
+const memory = config.requireNumber('memory')
+
+// Create a resource group.
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+
+// Create the container registry.
+const cleanRegistryName = `${prefixName}acr`.replace(/[^a-zA-Z0-9]/g, "");
+
+// Create the container registry using the sanitized name configuration.
+const registry = new containerregistry.Registry(cleanRegistryName, {
+  resourceGroupName: resourceGroup.name,
+  adminUserEnabled: true,
+  sku: {
+    name: containerregistry.SkuName.Basic,
+  },
+});
+
+const registryCredentials = containerregistry
+  .listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name,
+  })
+  .apply((creds) => {
+    return {
+      username: creds.username!,
+      password: creds.passwords![0].value!,
+    }
+  })
+
